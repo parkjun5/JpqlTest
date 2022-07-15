fcad9ba1b144dfcdc5cae62475ba97343ba8e520
박세준 <sjpark@selab.co.kr>
2022-07-15 15:55:49 +0900

    [FEA] fetch join distinct 처리 방안

--- src/main/java/main/jpaMain.java
+++ src/main/java/main/jpaMain.java
@@ -6,6 +6,7 @@ import domain.MemberType;
 import domain.Team;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 
@@ -18,54 +19,84 @@ public class jpaMain {
         tx.begin();
 
         try {
+            Team teamA= new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
-            Team team = new Team();
-            team.setName("selab");
-            em.persist(team);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
+            Team teamC = new Team();
+            teamC.setName("teamC");
+            em.persist(teamC);
 
             Member member = new Member();
-            member.setName("park");
+            member.setName("member1");
             member.setAge(26);
-            member.setAddress( Address.createAddress("수원", "파크로", "111-111"));
-            member.setTeam(team);
-            team.getMembers().add(member);
+            member.setAddress(Address.createAddress("수원", "파크로", "111-111"));
+            member.setTeam(teamA);
+            teamA.getMembers().add(member);
             em.persist(member);
 
-            Member mem2 = new Member();
-            mem2.setName("se");
-            mem2.setAge(28);
-            mem2.setMemberType(MemberType.ADMIN);
-            mem2.setAddress( Address.createAddress("수원", "파크로", "111-111"));
-            mem2.setTeam(team);
-            team.getMembers().add(mem2);
-            em.persist(mem2);
+            Member m2 = new Member();
+            m2.setName("member2");
+            m2.setAge(30);
+            m2.setAddress(Address.createAddress("수원", "d", "b"));
+            m2.changeTeam(teamA);
+            teamA.getMembers().add(m2);
+            em.persist(m2);
 
-//기본문법(em);
-//기본문법테스트(em);
-//서브쿼리(em);
+            Member member3 = new Member();
+            member3.setName("park");
+            member3.setAge(26);
+            member3.setAddress(Address.createAddress("수원", "파크로", "111-111"));
+            member3.setTeam(teamB);
+            teamB.getMembers().add(member3);
+            em.persist(member3);
+
+            Member member4 = new Member();
+            member4.setName("park");
+            member4.setAge(26);
+            member4.setAddress(Address.createAddress("수원", "파크로", "111-111"));
+            member4.setTeam(teamC);
+            teamC.getMembers().add(member4);
+            em.persist(member4);
+
+
+            System.out.println("========== 기 본 설 정 ============");
+//            기본문법(em);
+//            기본문법테스트(em);
+//            조인예제(em);
+
+//            String teamButInnerJoin = "select m.team from Member m";
 //            String innerJoin = "select m from Member m inner join m.team t";
 //            String outerJoin = "select m from Member m left join m.team t";
 //            String setaJoin = "select m from Member m, Team t where m.name = t.name";
-//
 //            String leftOnJoin = "select m from Member m left join m.team t on t.name='selab'";
-//            String setaOnJoin = "select m from Member m left join m.team t on m.name = t.name";
-//
 //            List<Member> resultList = em.createQuery(setaOnJoin, Member.class).getResultList();
 
-            String query = "select m.name, 'HELLO?', TRUE from Member m" +
-                    " where m.memberType = domain.MemberType.ADMIN";
-            List<Object[]> resultList = em.createQuery(query)
-                    .getResultList();
-            System.out.println("temp");
-            for (Object[] objects : resultList) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[0] = " + objects[1]);
-                System.out.println("objects[0] = " + objects[2]);
+            em.flush();
+            em.clear();
 
+            String selectMemberWithTeam = "select m from Member m join fetch m.team";
+            String selectTeamWithMembers = "select distinct t from Team t join fetch t.members";
+//            String query2 = "select m.name, 'HELLO?', TRUE from Member m" +
+//                    " where m.memberType = domain.MemberType.ADMIN";
+            List<Team> resultList = em.createQuery(selectTeamWithMembers, Team.class)
+                    .getResultList();
+
+            for (Team team : resultList) {
+//                System.out.println("member1.getName() +\", \" + member1.getTeam().getName() = " + member1.getName() +", " + member1.getTeam().getName());
+                System.out.println("team.getName() = " + team.getName() + "|| team.getMembers() = " + team.getMembers().size());
+                for (Member teamMember : team.getMembers()) {
+                    System.out.println("----> teamMember = " + teamMember);
+                }
+            
             }
 
-            tx.commit();
 
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
             e.printStackTrace();
@@ -74,6 +105,17 @@ public class jpaMain {
         }
 
         emf.close();
+    }
+
+    private static void 조인예제(EntityManager em) {
+        String innerJoin = "select m from Member m inner join m.team t";
+        String outerJoin = "select m from Member m left join m.team t";
+        String setaJoin = "select m from Member m, Team t where m.name = t.name";
+
+        String leftOnJoin = "select m from Member m left join m.team t on t.name='selab'";
+        String setaOnJoin = "select m from Member m left join m.team t on m.name = t.name";
+
+        List<Member> resultList = em.createQuery(setaOnJoin, Member.class).getResultList();
     }
 
     private static void 서브쿼리(EntityManager em) {
@@ -85,7 +127,7 @@ public class jpaMain {
             Member member = new Member();
             member.setName("park" + i);
             member.setAge(i);
-            member.setAddress( Address.createAddress("수원", "파크로", "111-111"));
+            member.setAddress(Address.createAddress("수원", "파크로", "111-111"));
             member.setTeam(team);
             team.getMembers().add(member);
             em.persist(member);
